44527477f9956dc2a4cef8c0a44d6a287bde8fd7
Andrej Pavlovic <andrej.pavlovic@massedge.com>
2019-08-26 09:42:03 -0400

    output es5 and es6 modules when building

--- webpack.config.js
+++ webpack.config.js
@@ -2,51 +2,55 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const PRODUCTION = process.env.NODE_ENV === 'production';
+module.exports = (env, argv) => {
+  const PRODUCTION = argv.mode === 'production';
 
-// plugins
-const plugins = [
-  new HtmlWebpackPlugin({
-    title: 'Development',
-    template: 'src/index.html',
-    inject: 'head',
-  })
-];
-
-if (!PRODUCTION) {
-  plugins.push(new webpack.NamedModulesPlugin());
-  plugins.push(new webpack.HotModuleReplacementPlugin());
-}
-
-module.exports = {
-  entry: path.resolve(__dirname, 'src/index.ts'),
-  devtool: "source-map",
-  module: {
-    rules: [{
-			test: /\.ts$/,
-      use: {
-        loader: 'babel-loader',
-      },
-			include: [
-				path.resolve(__dirname, 'src'),
-			]
-    }]
-  },
-  resolve: {
-    extensions: ['.ts', '.wasm', '.mjs', '.js', '.json']
-  },
-  plugins: plugins,
-  output: {
-    library: 'YacCarousel',
-    libraryTarget: 'umd',
-    filename: '[name].min.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    noInfo: true,
-    hot: true,
-    stats: {
-      colors: true
+  // plugins
+  const plugins = [];
+  
+  if (!PRODUCTION) {
+    plugins.push(new HtmlWebpackPlugin({
+      title: 'Development',
+      template: 'src/index.html',
+      inject: 'head',
+    }))
+  }
+  
+  if (!PRODUCTION) {
+    plugins.push(new webpack.NamedModulesPlugin());
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+  
+  return {
+    entry: path.resolve(__dirname, 'src/index.ts'),
+    devtool: "source-map",
+    module: {
+      rules: [{
+        test: /\.ts$/,
+        use: {
+          loader: 'babel-loader',
+        },
+        include: [
+          path.resolve(__dirname, 'src'),
+        ]
+      }]
+    },
+    resolve: {
+      extensions: ['.ts', '.wasm', '.mjs', '.js', '.json']
+    },
+    plugins: plugins,
+    output: {
+      library: 'YacCarousel',
+      libraryTarget: 'umd',
+      filename: '[name].min.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+      noInfo: true,
+      hot: true,
+      stats: {
+        colors: true
+      }
     }
   }
-};
+}
