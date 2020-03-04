256437b6a097172abb20cef62a5b99546748c199
Andrej Pavlovic <andrej.pavlovic@massedge.com>
2020-03-04 10:10:16 -0500

    webpack.config.js - devServer - ensure browser opens on localhost:8080 instead of 0.0.0.0:8080

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = (env, argv) => {
     devServer: {
       noInfo: true,
       hot: true,
-      host: '0.0.0.0',
+      // host: '0.0.0.0',
       stats: {
         colors: true
       }
