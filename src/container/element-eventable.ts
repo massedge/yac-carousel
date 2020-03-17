d16bf29695b53280d0109a881e86f92564d06d18
Andrej Pavlovic <andrej.pavlovic@massedge.com>
2020-03-17 11:34:22 -0400

    container/element-eventable.ts - don't expose calls to on/off directly, since these methods are supposed to be implementations of abstract functions, but not able to be called directly

--- src/container/element-eventable.ts
+++ src/container/element-eventable.ts
@@ -9,25 +9,23 @@ export interface ElementEventable {
 }
 
 export interface ElementEventableInstance {
-  on: (type: string, listener: EventListener) => void
-  off: (type: string, listener: EventListener) => void
 }
 
 export default function ElementEventable<T extends new (o: any) => any>(Base: T) {
   class Mixin extends (Base as new (...a: any[]) => {element: HTMLElement}) implements ElementEventableInstance {
-      static readonly elementEventable = true
+    static readonly elementEventable = true
 
-      on(type: string, listener: EventListener) {
-        this.element.addEventListener(type, listener)
-      }
-    
-      off(type: string, listener: EventListener) {
-        this.element.removeEventListener(type, listener)
-      }
+    protected on(type: string, listener: EventListener) {
+      this.element.addEventListener(type, listener)
+    }
+  
+    protected off(type: string, listener: EventListener) {
+      this.element.removeEventListener(type, listener)
+    }
 
-      protected _emit(e: Event) {
-        this.element.dispatchEvent(e)
-      }
+    protected _emit(e: Event) {
+      this.element.dispatchEvent(e)
+    }
   }
   
   return Mixin as unknown as ComposeConstructor<ElementEventable, typeof Base>
