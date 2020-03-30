28644e2c35a782c1a4f8255909805dd187d6643d
Andrej Pavlovic <andrej.pavlovic@massedge.com>
2020-03-30 00:43:07 -0400

    added jest testing dependencies and configuration

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: [
+    '<rootDir>/src'
+  ],
+  transform: {
+    '.(ts|tsx)': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx|js)?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
